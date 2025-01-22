4ae64e334aa116fae621ab4bf2902120c98d6f98
Mateusz Nowak <mateusz@dddheroes.com>
2025-01-23 00:29:42 +0100

    📦️ deps: configure Spring Boot + Axon Framework (#5)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,62 @@
 plugins {
-    kotlin("jvm") version "2.1.0"
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.4.1"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.dddheroes"
-version = "1.0-SNAPSHOT"
+version = "0.0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
+ext {
+    set("axonVersion", "4.10.3")
+    set("assertkVersion", "0.28.1")
+}
+
 dependencies {
+    val axonVersion: String by project
+    val assertkVersion: String by project
+
     testImplementation(kotlin("test"))
-    testImplementation("com.willowtreeapps.assertk:assertk:0.28.1")
+    testImplementation("com.willowtreeapps.assertk:assertk:$assertkVersion")
+
+    // - Spring Boot
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // - Axon Framework
+    implementation(platform("org.axonframework:axon-bom:$axonVersion"))
+    implementation("org.axonframework:axon-modelling")
+    testImplementation("org.axonframework:axon-test")
+
+    // ---- if (use Axon Framework Spring Boot Starter) ----
+    implementation("org.axonframework:axon-spring-boot-starter")
+    // ---- else ----
+    // implementation("org.axonframework:axon-configuration")
+    // implementation("org.axonframework:axon-server-connector")
+    // testImplementation("org.axonframework:axon-test")
+    // ---- endif -----
+
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
 }
 
 tasks.test {
---
+++ src/main/kotlin/com/dddheroes/heroesofddd/HeroesOfDDDApplication.kt
@@ -0,0 +1,11 @@
+package com.dddheroes.heroesofddd
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HeroesOfDDDApplication
+
+fun main(args: Array<String>) {
+    runApplication<HeroesOfDDDApplication>(*args)
+}
